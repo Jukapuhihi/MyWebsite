302b63b171878bc9305191444ab8c45e90ec5445
Le Dung <ledungjuka@gmail.com>
2019-04-21 19:48:32 +0700

    change guess's site

--- public/js/home.js
+++ public/js/home.js
@@ -36,26 +36,32 @@ $(document).ready(function() {
         }, 1000);
     })
 
-    $("a").click(function(event) {
-        event.preventDefault(); // prevent load page, change location (url) => ko có hash trên url
-        const hash = this.hash;
-        $("body, html").animate({
-            scrollTop: $(hash).offset().top
-        }, 600);
-        // thêm hash cho location
-        location.hash = hash;
+    // $("a").click(function(event) {
+    //     event.preventDefault(); // prevent load page, change location (url) => ko có hash trên url
+    //     const hash = this.hash;
+    //     $("body, html").animate({
+    //         scrollTop: $(hash).offset().top
+    //     }, 600);
+    //     // thêm hash cho location
+    //     location.hash = hash;
 
-        // loại đi thằng đang nắm active class
-        $(".nav__item").removeClass("nav__item--active"); // ko còn thằng nav__item nà nắm activee
+    //     // loại đi thằng đang nắm active class
+    //     $(".nav__item").removeClass("nav__item--active"); // ko còn thằng nav__item nà nắm activee
 
-        // thêm class active cho thằng mình mới click
-        // thằng hiện tại đang chọn là thằng a, nhưng mình muốn thêm active cho thằng cha (nav__item)
-        $(this).parent().addClass("nav__item--active");
-    });
+    //     // thêm class active cho thằng mình mới click
+    //     // thằng hiện tại đang chọn là thằng a, nhưng mình muốn thêm active cho thằng cha (nav__item)
+    //     $(this).parent().addClass("nav__item--active");
+    // });
 
     function onWindowScroll() {
+        if (wd.scrollTop() <= 60) {
+            $(".header__pagename").css("color","white");
+            $("#btn-cart i").css("color","white");
+        }
         if ( wd.scrollTop() > 60 ) {
             $(".nav").removeClass("nav--transparent");
+            $(".header__pagename").css("color","var(--primary)");
+            $("#btn-cart i").css("color","var(--primary)");
         }
         else {
             $(".nav").addClass("nav--transparent");
